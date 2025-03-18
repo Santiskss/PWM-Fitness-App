f35e4c78a54401b8e2ebdb5caf016faecdbb2700
Santiskss <santitareas19@gmail.com>
2025-03-18 18:24:36 +0000

    Implementación de script.js

---
+++ JavaScript/script.js
@@ -0,0 +1,9 @@
+function loadTemplate(templatePath, elementId) {
+    fetch(templatePath)
+        .then(resp => resp.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => console.error(`Error cargando ${templatePath}:`, error));
+    console.log("Hola")
+}
