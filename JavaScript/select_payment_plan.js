3b9b66bc2067859a68a6e987e2f2a7e49e7f1d8b
Magickio <dad2dadad@gmail.com>
2025-03-29 17:59:46 +0000

    Dynamic loading of the select_payment_plan page

---
+++ JavaScript/select_payment_plan.js
@@ -0,0 +1,29 @@
+
+function load()
+{
+    loadHeader()
+    fetch("../JSON/data.json").then(function(res)
+    {
+        return res.json();
+    })
+        .then(function (json)
+        {
+            let rectangles = document.querySelector(".rectangles")
+            rectangles.innerHTML = "<div class='sub' id='subscription1'></div><div class='sub' id='subscription2'></div><div class='sub' id='subscription3'></div>"
+            Promise.all([loadTemplate("/Templates/subscription.html", "subscription1"),
+            loadTemplate("/Templates/subscription.html", "subscription2"),
+            loadTemplate("/Templates/subscription.html", "subscription3")]
+            ).then(() => {
+                let questions = document.querySelectorAll("th");
+                let prices = document.querySelectorAll("#price");
+                for (let i = 0; i < prices.length; i++) {
+                    document.querySelectorAll("#price")[i].textContent = json.suscripciones.planes[i].precio
+                }
+                for (let i = 0; i < questions.length; i++) {
+                    questions[i].textContent = json.suscripciones.preguntas[i].pregunta
+                }
+            })
+        })
+    loadFooter()
+}
+
