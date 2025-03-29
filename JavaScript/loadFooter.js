1e9adc180b60c904298b4ae25f33e05762b354f1
Magickio <dad2dadad@gmail.com>
2025-03-29 03:12:28 +0000

    Dynamic loading of the pro_rutine page

--- JavaScript/loadFooter.js
+++ JavaScript/loadFooter.js
@@ -6,7 +6,6 @@ function loadFooter()
         .then(function (myJson)
         {
             let texto_footer = document.getElementsByClassName("footer_texts")
-            console.log(texto_footer)
             for (let i=0; i<texto_footer.length; i++)
             {
                 texto_footer[i].textContent = myJson.footer_text[i]
@@ -16,13 +15,10 @@ function loadFooter()
             for (let i = 0; i < texto_footer2.length; i++) {
                 texto_footer2[i].textContent = myJson.company_footer[i]
             }
-            console.log(myJson.redes_sociales_footer)
             let otro = document.getElementsByClassName("footer_temp")[0].querySelectorAll("div")[2].querySelectorAll("img")
             for (let i=0; i<otro.length; i++)
             {
                 otro[i].src = myJson.redes_sociales_footer[i].imagen
-                otro[i].style.width = "2em"
-                otro[i].style.height = "2em"
                 otro[i].addEventListener("click", function()
                 {
                     window.open(myJson.redes_sociales_footer[i].link);
---
+++ JavaScript/pro_rutine.js
@@ -0,0 +1,18 @@
+function load()
+{
+    loadHeader()
+    fetch("../JSON/routines_core.json").then(function(res)
+    {
+        return res.json();
+    })
+        .then(function (json)
+        {
+            const rutinas = document.querySelectorAll(".info")
+            for (let i = 0; i < rutinas.length; i++)
+            {
+                rutinas[i].textContent = json[i*3].description
+
+            }
+        })
+    loadFooter()
+}
