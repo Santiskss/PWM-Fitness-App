82b800c9b5e2030015b835a5bcc0b4c856f74e9d
Magickio <dad2dadad@gmail.com>
2025-03-30 22:12:17 +0100

    load images on first page

--- JavaScript/first_page.js
+++ JavaScript/first_page.js
@@ -10,6 +10,8 @@ function load()
         {
             document.querySelectorAll("#info")[0].textContent = json.first_page.today
             document.querySelectorAll("#info")[1].textContent = json.first_page.calendar
+            document.querySelectorAll("#picture")[0].src = json.first_page.today_img;
+            document.querySelectorAll("#picture")[1].src = json.first_page.calendar_img;
         })
     loadFooter()
 }
