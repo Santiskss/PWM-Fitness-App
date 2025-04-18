6044b16b9726260f2df2c185a2248b06ef0df2b9
maria <informatica1bachIES@gmail.com>
2025-04-18 10:51:19 +0100

    Angular components created (login and profile)

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import {MainPageComponent} from './components/main-page/main-page.component';
 import {EditRutineComponent} from './components/edit-rutine/edit-rutine.component';
 import {ProRutineComponent} from './components/pro-rutine/pro-rutine.component';
 import {CreateRutineComponent} from './components/create-rutine/create-rutine.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {LoginComponent} from './components/login/login.component';
 
 export const routes: Routes = [
   {path: '', title: "Main Page", component: MainPageComponent},
   {path: 'edit-rutine', title: "Main Page", component: EditRutineComponent},
   {path: 'pro-rutine', title: "Main Page", component: ProRutineComponent},
   {path: 'create-rutine', title: "Main Page", component: CreateRutineComponent},
+  {path: 'profile', title: "Main Page", component: ProfileComponent},
+  {path: 'login', title: "Main Page", component: LoginComponent},
 ];
---
+++ angular/src/app/components/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from '../header/header.component';
+import {FooterComponent} from '../footer/footer.component';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    HeaderComponent,
+    FooterComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
