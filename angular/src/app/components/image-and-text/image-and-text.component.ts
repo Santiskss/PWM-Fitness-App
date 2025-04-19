44898f2a979f726506a2876aa65a380dd04c09c4
mohamed <elouariachimohamed2002@gmail.com>
2025-04-19 18:44:14 +0200

    Create templates on Angular

---
+++ angular/src/app/components/image-and-text/image-and-text.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-and-text',
+  imports: [],
+  templateUrl: './image-and-text.component.html',
+  styleUrl: './image-and-text.component.css'
+})
+export class ImageAndTextComponent {
+
+}
